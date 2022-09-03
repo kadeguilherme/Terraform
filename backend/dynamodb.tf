ec42510eb4d3a0044792b0abebc8c192bcbe3c1f
kadeguilherme <kadeguilherme@gmail.com>
2022-09-03 16:36:54 -0300

    Criando banco de dado dynamodb e populando o banco

--- backend/dynamodb.tf
+++ backend/dynamodb.tf
@@ -1,19 +1,48 @@
-resource "aws_dynamodb_table" "basic-dynamodb-lock" {
-    name = "basic-dynamodb-lock"
+resource "aws_dynamodb_table" "dynamodb-bank" {
+    name = "DataBbase_Bank"
+    billing_mode   = "PROVISIONED"
     read_capacity  = 20
     write_capacity = 20
-    hash_key = "UserId"
-  
-  attribute {
-    name = "UserId"
-    type = "S"
+    hash_key = "cpf"
     
-}
-
-    tags = {
-    Name = "dynamodb-table-1"
-
+  attribute {
+    name = "cpf"
+    type = "N"
   }
 
+
+    tags = {
+    Name = "ProductCatalog-tag"
+  }
+
+}
+
+
+resource "aws_dynamodb_table_item" "account" {
+  table_name = aws_dynamodb_table.dynamodb-bank.name
+  hash_key = aws_dynamodb_table.dynamodb-bank.hash_key
+
+  for_each = {
+  "1" = { 
+    name = "Guilherme"
+    age = 20
+    }
+  "2" = { 
+    name = "Jose"
+    age = 10
+    }
+  "3" = {
+    name = "Camila"
+    age = 54
+  }
+  }
   
+
+  item = <<ITEM
+  {
+    "cpf": {"N": "${ each.key }"},
+    "name" : {"S" : "${ each.value.name }"},
+    "age" : {"N" : "${ each.value.age }"}
+  }
+  ITEM
 }
--- backend/main.tf
+++ backend/main.tf
@@ -2,13 +2,3 @@ provider "aws" {
   region     = "us-east-1"
 
 }
-
-terraform {
-  backend "s3" {
-    bucket = "bucket-ambiente-desenvolvimento-dev"
-    dynamodb_table = "basic-dynamodb-lock"
-    key = "terraform-test.tfstate"
-    
-    region = "us-east-1"
-  }
-}
